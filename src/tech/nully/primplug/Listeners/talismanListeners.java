67f8d2cb2dd7a0a20e75e5801369e291732bd475
primboi <prianshmittra@gmail.com>
2022-04-10 19:00:42 -0700

    Add file writing to "Curse of Dispair"

--- src/tech/nully/primplug/Listeners/talismanListeners.java
+++ src/tech/nully/primplug/Listeners/talismanListeners.java
@@ -9,13 +9,25 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import java.nio.file.Files;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*; 
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.nio.file.StandardOpenOption;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
 
 import tech.nully.primplug.Items.Talisman;
 
 public class talismanListeners implements Listener {
     static Player p;
     @EventHandler
-    public static void OnRightClickTalisman(PlayerInteractEvent i) {
+    public static void OnRightClickTalisman(PlayerInteractEvent i) throws IOException {
         p = i.getPlayer();
         ItemStack handItem = p.getItemInHand();
         ItemMeta handItemMeta = handItem.getItemMeta();
@@ -24,7 +36,7 @@ public class talismanListeners implements Listener {
         if (p.getItemInHand().getType() == Material.RED_ROSE) {
             if (i.getAction() == Action.RIGHT_CLICK_AIR) {
                     if (handItemMeta.getDisplayName() == Talisman.BladeOfDespair.getItemMeta().getDisplayName()) {
-                        // TODO MAKE IT ADD TO CONFIG THING
+                        // TODO MAKE IT ADD TO CONFIG THING - idk if this works, bongo, commit ur code. -Null
                         p.getInventory().remove(Talisman.BladeOfDespair);
                         p.sendMessage(ChatColor.YELLOW + 
                         "Successfully added your" +
@@ -33,6 +45,25 @@ public class talismanListeners implements Listener {
                          " Curse of Despair " + 
                          ChatColor.YELLOW + 
                          "Talisman to your Talisman Bag!");
+                         System.out.println("Enter player name: ");
+                         String name = p.getName();  
+                         Path path = Paths.get("./talismans/"+name);
+                         if(!Files.exists(path)){
+                             Files.createDirectory(path);
+                         }
+                         // create a file at the path
+                         File file = new File("./talismans/"+name+"/talisman.txt");
+                         // if file doesnt exists, then create it
+                         if (!file.exists()) {
+                             file.createNewFile();
+                         }
+                         System.out.println("Enter the name of the talisman: ");
+                         String talisman = "Curse of Despair";
+                         ArrayList<String> finaldraft = new ArrayList<String>();
+                         finaldraft.add(talisman);
+                         FileWriter writer = new FileWriter(file, true);
+                         writer.write(talisman + "\n");
+                         writer.close();
                     }
             }
             // prevent talisman from being placed
