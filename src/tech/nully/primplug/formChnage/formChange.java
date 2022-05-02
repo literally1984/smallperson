4cd4bb2d05e005370e3600f79ac066f5a1e99746
BongoCat <Eseanfang@gmail.com>
2022-05-02 02:41:31 +0000

    changed form change a bit

--- src/tech/nully/primplug/formChnage/formChange.java
+++ src/tech/nully/primplug/formChnage/formChange.java
@@ -2,10 +2,16 @@ package tech.nully.primplug.formChnage;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class formChange implements Listener {
     public void changeForm(PlayerInteractEvent e) {
+        if (e.getAction() == Action.RIGHT_CLICK_AIR) {
+            if (e.getPlayer().isSneaking()) {
+                
+            }
+        }
     }
     
 }
