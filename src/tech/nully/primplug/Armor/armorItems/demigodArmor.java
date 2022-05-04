cfde5de7ec24536b5f62ca2f4458eb2792e900e0
BongoCat <Eseanfang@gmail.com>
2022-05-04 18:14:58 +0000

    jesus fucks

--- src/tech/nully/primplug/Armor/armorItems/demigodArmor.java
+++ src/tech/nully/primplug/Armor/armorItems/demigodArmor.java
@@ -10,7 +10,6 @@ import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.DiamondArmorMeta;
 
 
 public class demigodArmor {
@@ -23,7 +22,7 @@ public class demigodArmor {
     public static void init() {
         createDemiShard();
         createDemiHelm();
-        createdemiChestplate();
+        createDemiChestplate();
         createDemiLeggings();
         createDemiBoots();
     }
