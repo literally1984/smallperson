2be06961041af9c9fa7a54ed6a7b454afb3a5e3b
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-09 06:01:50 -0700

    Update enchantMechanic.java

--- src/tech/nully/primplug/enchantStuff/enchantMechanic.java
+++ src/tech/nully/primplug/enchantStuff/enchantMechanic.java
@@ -1,7 +1,25 @@
 package tech.nully.primplug.enchantStuff;
 
+import java.util.List;
+
+import org.bukkit.inventory.ItemStack;
+
 public class enchantMechanic {
+    private int getPossibleEncLevels(String enchant) {
+        if (enchant.equals("Sharpness") || enchant.equals("Smite") || enchant.equals("Efficiency")) {
+            return 5;
+        }
+    }
+
+    private final String[] possibleSwordEnchants = {};
     public void applyEnchants(ItemStack i) {
-        
+        List<String> lore = i.getItemMeta().getLore()
+        int indexOf = lore.indexOf(lore.get(lore.size() - 1));
+
+        while (!(lore.get(indexOf).equals(""))) {
+            indexOf = indexOf -1;
+        }
+
+
     }
 }
