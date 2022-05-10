b9a36514c3de3d7c6d5bcdde5292168da13810a2
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-10 07:42:50 -0700

    Update enchantMechanic.java

--- src/tech/nully/primplug/enchantStuff/enchantMechanic.java
+++ src/tech/nully/primplug/enchantStuff/enchantMechanic.java
@@ -1,26 +1,57 @@
 package tech.nully.primplug.enchantStuff;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import org.bukkit.inventory.ItemStack;
+import tech.nully.primplug.baseMethods;
 
 public class enchantMechanic {
+    // makes a method that prints out rick roll lyrics
+
     private int getPossibleEncLevels(String enchant) {
         if (enchant.equals("Sharpness") || enchant.equals("Smite") || enchant.equals("Efficiency")) {
             return 5;
         } else return 69;
     }
+    private final String[] possibleSwordEnchants = {"Sharpness", "Smite",};
+    private static HashMap<String, Integer> enchantLevels = new HashMap<String, Integer>();
 
-    private final String[] possibleSwordEnchants = {};
-    public void applyEnchants(ItemStack i) {
+    public static void createEnchantHashMap() {
+        enchantLevels.put("Sharpness", 5);
+        enchantLevels.put("Smite", 5);
+        enchantLevels.put("Bane of Arthropods", 5);
+        enchantLevels.put("Fortune", 3);
+        enchantLevels.put("Efficiency", 5);
+        enchantLevels.put("Fire Aspect", 2);
+        enchantLevels.put("Power", 5);
+        enchantLevels.put("Blast Protection", 5);
+        enchantLevels.put("Protection", 5);
+        enchantLevels.put("Projectile Protection", 5);
+        enchantLevels.put("Sharpness", 5);
+        enchantLevels.put("Feather Falling", 4);
+        enchantLevels.put("Sharpness", 5);
+    }
+    public void applyEnchants(ItemStack item) {
         // gets the Item's lore and gets the line which the rarity is stated
-        List<String> lore = i.getItemMeta().getLore();
+        List<String> lore = item.getItemMeta().getLore();
         int indexOf = lore.indexOf(lore.get(lore.size() - 3));
 
         // while the current data in indexOf is not blank, interate the line before it
         while (!(lore.get(indexOf).equals(""))) {
             indexOf = indexOf - 1;
         }
+        Random rand = new Random();
+        for (int i = 0; i < rand.nextInt(4); i++) {
+            if (item.getType().toString().equalsIgnoreCase("sword")) {
+                lore.add(indexOf, );
+            }
+
+        }
+
+
+        lore.add(indexOf, "");
 
 
     }
