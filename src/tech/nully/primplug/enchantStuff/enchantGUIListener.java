d57ca0d8c756fb4843e10ade6fd37f8b393307d3
BongoCat <Eseanfang@gmail.com>
2022-05-11 22:30:48 +0000

    updated enchanting stuff and added started spells

--- src/tech/nully/primplug/enchantStuff/enchantGUIListener.java
+++ src/tech/nully/primplug/enchantStuff/enchantGUIListener.java
@@ -1,7 +1,6 @@
 package tech.nully.primplug.enchantStuff;
 
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
--- src/tech/nully/primplug/magic/spellCast.java
+++ src/tech/nully/primplug/magic/spellCast.java
@@ -1,5 +1,37 @@
 package tech.nully.primplug.magic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class spellCast {
+
+    private static List<String> fireballLore = new ArrayList<String>();
+    private static List<String> explosionLore = new ArrayList<String>();
+    private static List<String> thousandCutsLore = new ArrayList<String>();
+    private static List<String> ArrowRainLore = new ArrayList<String>();
+    private static List<String> meteorShowerLore = new ArrayList<String>();
     
+    private static ItemStack fireballSpell;
+
+    public boolean IsSpell(ItemStack item) {
+        String dispName = item.getItemMeta().getDisplayName();
+        if (fireballSpell.getItemMeta().getLore() == fireballLore) {
+            return true;
+        }
+        return false;
+    }
+
+    public void createSpells() {
+        ItemStack fireball = new ItemStack(Material.FIREBALL);
+        ItemMeta fireballMeta = fireball.getItemMeta();
+
+        fireballMeta.setDisplayName(ChatColor.GOLD + "FIREBALL");
+        fireballLore.add(ChatColor.YELLOW + "Right click to cast a fireball spell");
+
+    }
 }
