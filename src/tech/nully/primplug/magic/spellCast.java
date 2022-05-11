68ca8ed470c2422d877fe23962f264feed100da4
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-11 16:21:37 -0700

    added some basic spell stuff

--- src/tech/nully/primplug/magic/spellCast.java
+++ src/tech/nully/primplug/magic/spellCast.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
 
 public class spellCast {
 
@@ -33,5 +36,49 @@ public class spellCast {
         fireballMeta.setDisplayName(ChatColor.GOLD + "FIREBALL");
         fireballLore.add(ChatColor.YELLOW + "Right click to cast a fireball spell");
 
+    }
+
+
+    public void castFireBall(Player p) {
+        double pitch = ((p.getLocation().getPitch() + 90) * Math.PI) / 180;
+        double yaw = ((p.getLocation().getYaw() + 90) * Math.PI) / 180;
+
+        double x = Math.sin(pitch) * Math.cos(yaw);
+        double y = Math.sin(pitch) * Math.sin(yaw);
+        double z = Math.cos(pitch);
+
+
+        Location spawnAt = p.getEyeLocation().toVector().add(p.getEyeLocation().getDirection()).toLocation(p.getWorld());
+        Fireball spell = p.getWorld().spawn(spawnAt, Fireball.class);
+
+        Vector vector = new Vector(x, z, y);
+
+        spell.setDirection(vector.multiply(10));
+        spell.setBounce(false);
+        spell.setIsIncendiary(false);
+        spell.setYield(4F);
+
+
+    }
+
+
+    public void castArrowRain(Player p) {
+        Location loc = p.getEyeLocation().toVector().add(p.getEyeLocation().getDirection()).toLocation(p.getWorld());
+        Arrow arrow = (Arrow)p.getWorld().spawn(loc.clone().add(x, y, z), Arrow.class);
+        outerloop: for (double i = 0.5; i < 12; i+=0.5){ //This will rapidly increase the range so you don't get so many mobs at one, and instead checks a few blocks at a time
+            for (Entity e : arrow.getNearbyEntities(i, 3, i)){ //Gets ALL nearby entities using the loop variable above it
+                if (e != arrow.getShooter()){ //Checks to make sure the entities isn't the shooter
+                    if (e.getType().isAlive()){ //Checks to make sure the entity is alive
+                        Location from = arrow.getLocation(); //Gets the arrows location
+                        Location to = e.getLocation(); //Gets the entities Location
+                        Vector vFrom = from.toVector(); //Converts the from location to a vector
+                        Vector vTo = to.toVector(); //Converts the to location to a vector
+                        Vector direction = vTo.subtract(vFrom).normalize(); //Subtracts the to variable to the from variable and normalizes it.
+                        arrow.setVelocity(direction); //Sets the arrows newfound direction
+                        break outerloop;
+                    }
+                }
+            }
+        }
     }
 }
--- src/tech/nully/primplug/magic/spellCastTrigger.java
+++ src/tech/nully/primplug/magic/spellCastTrigger.java
@@ -1,14 +1,14 @@
 package tech.nully.primplug.magic;
 
-import java.net.http.WebSocket.Listener;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class spellCastTrigger implements Listener{
+public class spellCastTrigger implements Listener {
     @EventHandler
     public void onSpellCast(PlayerInteractEvent e) {
-        
+
     }
     
 }
