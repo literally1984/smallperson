96429894512abdc43e2da4fa714cc4b372f3800f
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-13 16:09:16 -0700

    Created spell items with Fireball scroll and spell

---
+++ src/tech/nully/primplug/magic/spellItems.java
@@ -0,0 +1,67 @@
+package tech.nully.primplug.magic;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import tech.nully.primplug.Armor.baseAttributesAdder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class spellItems {
+
+    public static void init() {
+        makeSpells();
+        makeSpellScrolls();
+    }
+
+    static baseAttributesAdder b = new baseAttributesAdder();
+
+    private static void makeSpells() {
+        ItemStack FireballSpell = new ItemStack(Material.FIREBALL);
+        ItemMeta FireballMeta = FireballSpell.getItemMeta();
+
+        FireballMeta.setDisplayName(ChatColor.GOLD + "Fireball Spell");
+        List<String> FireballItemLore = new ArrayList<String>();
+        FireballItemLore.add("");
+        FireballItemLore.add(ChatColor.GRAY + "Spell Item");
+        FireballItemLore.add("");
+        FireballItemLore.add(ChatColor.GRAY + "Scales 100% on Magic Damage");
+        FireballItemLore.add(ChatColor.RED + "Base Damage:" + ChatColor.GRAY + "6");
+        FireballItemLore.add("");
+        FireballItemLore.add("Launches a fireball in the direction that you");
+        FireballItemLore.add("look at, causing a an explosion.");
+        FireballItemLore.add("");
+        FireballItemLore.add(b.uncommon() + "UNCOMMON");
+
+        FireballMeta.setLore(FireballItemLore);
+
+    }
+
+    private static void makeSpellScrolls() {
+        ItemStack FireballScroll = new ItemStack(Material.FIREBALL);
+        ItemMeta FireballScrollMeta = FireballScroll.getItemMeta();
+        List<String> FireballLore = new ArrayList<String>();
+
+        FireballScrollMeta.setDisplayName("Fireball Scroll");
+        FireballLore.add("");
+        FireballLore.add(ChatColor.GRAY + "Spell Scroll Item");
+        FireballLore.add("");
+        FireballLore.add(ChatColor.GRAY + "Scales 100% on Magic Damage");
+        FireballLore.add(ChatColor.RED + "Base Damage:" + ChatColor.GRAY + "6");
+        FireballLore.add("");
+        FireballLore.add(ChatColor.GRAY + "Right Click the air to learn this spell");
+        FireballLore.add(ChatColor.GREEN + "Levels needed to learn: 21");
+        FireballLore.add("");
+        FireballLore.add("Launches a fireball in the direction that you");
+        FireballLore.add("look at, causing a an explosion.");
+        FireballLore.add("");
+        FireballLore.add(b.uncommon() + "UNCOMMON");
+
+        FireballScrollMeta.setLore(FireballLore);
+
+
+
+    }
+}
