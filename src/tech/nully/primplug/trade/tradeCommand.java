968400e134eaf2766b8733cc6df88ac365da0a46
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-14 22:12:45 -0700

    almost completed trade command handler, need to make tradeGUI

--- src/tech/nully/primplug/trade/tradeCommand.java
+++ src/tech/nully/primplug/trade/tradeCommand.java
@@ -1,4 +1,47 @@
 package tech.nully.primplug.trade;
 
-public class tradeCommand {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class tradeCommand implements CommandExecutor {
+    HashMap<Player, Player> activeRequests = new HashMap<Player, Player>();
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!(sender instanceof Player)) {
+            return false;
+        }
+
+        // Command name chec, argument length less than 2, args[0] is an onlone player
+        if(cmd.getName().equalsIgnoreCase("trade") && args.length <2 && Bukkit.getPlayer(args[0]) != null) {
+            Player requester = (Player) sender;
+            Player requestee = Bukkit.getPlayer(args[0]);
+
+            requester.sendMessage(ChatColor.YELLOW + "You requested to trade with" + requestee.getDisplayName());
+            requestee.sendMessage(ChatColor.LIGHT_PURPLE + "--------------------------------------------------------");
+            requestee.sendMessage(ChatColor.LIGHT_PURPLE + "--------------------------------------------------------");
+            requestee.sendMessage(ChatColor.YELLOW + requester.getDisplayName() + ChatColor.GREEN + "has requested to trade with you, type /taccept to accept");
+            requestee.sendMessage(ChatColor.LIGHT_PURPLE + "--------------------------------------------------------");
+            requestee.sendMessage(ChatColor.LIGHT_PURPLE + "--------------------------------------------------------");
+
+            activeRequests.put(requestee, requester);
+            return true;
+        }
+
+        // -------------- Handler for the accept command ----------------
+        if (cmd.getName().equalsIgnoreCase("taccept") && args.length <2 && Bukkit.getPlayer(args[0]) != null) {
+            Player requestAccepter = (Player) sender;
+            if (activeRequests.containsKey(requestAccepter)) {
+                Player p1 = activeRequests.get(requestAccepter);
+                // TODO OPEN TRADE GUI TO requestAccepter and p1
+            }
+            return true;
+        }
+        return false;
+    }
 }
--- src/tech/nully/primplug/trade/tradeGUIManager.java
+++ src/tech/nully/primplug/trade/tradeGUIManager.java
@@ -1,4 +1,5 @@
 package tech.nully.primplug.trade;
 
 public class tradeGUIManager {
+
 }
