46b06bd747f7ecc03ad55159bbd562063d7686c9
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-15 18:06:29 -0700

    added the crafting stuff

---
+++ src/tech/nully/primplug/crafting/customRecipes/armorRecipes.java
@@ -0,0 +1,317 @@
+package tech.nully.primplug.crafting.customRecipes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.material.MaterialData;
+import tech.nully.primplug.Armor.armorItems.PetheriteSet;
+import tech.nully.primplug.Armor.armorItems.cactusArmor;
+import tech.nully.primplug.Tools.zeusBolt;
+import tech.nully.primplug.crafting.overridenRecipes.overridenRecipeItems;
+
+public class armorRecipes {
+
+    // TODO: ADD THE NEW ARMOR ITEMS CRAFTING RECIPES -BigZ
+
+    public static void init() {
+        cactusChesRecipe();
+        PetheriteChesRecipe();
+        diaChesRecipe();
+        ironChesRecipe();
+        demichesrecipe();
+        drakonchesrecipe();
+        hdiachesrecipe();
+        lapischesrecipe();
+        platedchesrecipe();
+        reaperchesrecipe();
+        undeadchesrecipe();
+        wizchesrecipe();
+
+
+        cactusHelmRecipe();
+        PetheriteHelmRecipe();
+        diaHelmRecipe();
+        ironHelmRecipe();
+        demihelmrecipe();
+        drakonhelmrecipe();
+        hdiahelmrecipe();
+        lapishelmrecipe();
+        platedhelmrecipe();
+        reaperhelmrecipe();
+        undeadhelmrecipe();
+        wizhatrecipe();
+
+
+        cactusLegRecipe();
+        PetheriteLegRecipe();
+        diaLegRecipe();
+        ironLegRecipe();
+        demiLegrecipe();
+        drakonlegrecipe();
+        hdialegrecipe();
+        lapislegrecipe();
+        platedlegrecipe();
+        reaperlegrecipe();
+        undeadlegrecipe();
+        wizlegrecipe();
+
+
+        cactusBootsRecipe();
+        PetheriteBootsRecipe();
+        diaBootsRecipe();
+        ironBootsRecipe();
+        demibootsrecipe();
+        drakonbootsrecipe();
+        hdiabootsrecipe();
+        lapisbootsrecipe();
+        platedbootsrecipe();
+        reaperbootsrecipe();
+        undeadbootsrecipe();
+        wizbootsrecipe();
+    }
+
+// TODO: add recipe for new armors -BigZ
+
+   private static void wizbootsrecipe() {
+    }
+
+private static void undeadbootsrecipe() {
+    }
+
+private static void reaperbootsrecipe() {
+    }
+
+private static void platedbootsrecipe() {
+    }
+
+private static void lapisbootsrecipe() {
+    }
+
+private static void hdiabootsrecipe() {
+    }
+
+private static void drakonbootsrecipe() {
+    }
+
+private static void demibootsrecipe() {
+    }
+
+private static void wizlegrecipe() {
+    }
+
+private static void undeadlegrecipe() {
+    }
+
+private static void reaperlegrecipe() {
+    }
+
+private static void platedlegrecipe() {
+    }
+
+private static void lapislegrecipe() {
+    }
+
+private static void hdialegrecipe() {
+    }
+
+private static void drakonlegrecipe() {
+    }
+
+private static void demiLegrecipe() {
+    }
+
+private static void wizhatrecipe() {
+    }
+
+private static void undeadhelmrecipe() {
+    }
+
+private static void reaperhelmrecipe() {
+    }
+
+private static void platedhelmrecipe() {
+    }
+
+ private static void lapishelmrecipe() {
+    }
+
+    private static void hdiahelmrecipe() {
+    }
+
+    private static void drakonhelmrecipe() {
+    }
+
+    private static void demihelmrecipe() {
+    }
+
+    private static void wizchesrecipe() {
+    }
+
+    private static void undeadchesrecipe() {
+    }
+
+    private static void reaperchesrecipe() {
+    }
+
+    private static void platedchesrecipe() {
+    }
+
+    private static void lapischesrecipe() {
+    }
+
+    private static void hdiachesrecipe() {
+    }
+
+    private static void drakonchesrecipe() {
+    }
+
+    private static void demichesrecipe() {
+    }
+
+    private static void cactusHelmRecipe() {
+        ShapedRecipe cacHelm = new ShapedRecipe(cactusArmor.helm);
+
+        cacHelm.shape("^^^", "^ ^", "   ");
+        cacHelm.setIngredient('^', new MaterialData(Material.CACTUS, (byte) 1));
+        Bukkit.getServer().addRecipe(cacHelm);
+    }
+
+    private static void PetheriteHelmRecipe() {
+        ShapedRecipe PHelm = new ShapedRecipe(PetheriteSet.helm);
+
+        PHelm.shape("^^^", "^ ^", "   ");
+        PHelm.setIngredient('^', new MaterialData(Material.OBSIDIAN, (byte) 1));
+        Bukkit.getServer().addRecipe(PHelm);
+    }
+
+    private static void diaHelmRecipe() {
+        ShapedRecipe diaHelm = new ShapedRecipe(overridenRecipeItems.diaHelm);
+
+        diaHelm.shape("^^^", "^ ^", "   ");
+        diaHelm.setIngredient('^', Material.DIAMOND);
+        Bukkit.getServer().addRecipe(diaHelm);
+    }
+
+    private static void ironHelmRecipe() {
+        ShapedRecipe ironHelm = new ShapedRecipe(overridenRecipeItems.ironBoots);
+
+        ironHelm.shape("^^^", "^ ^", "   ");
+        ironHelm.setIngredient('^', Material.IRON_INGOT);
+        Bukkit.getServer().addRecipe(ironHelm);
+        // TODO: fix the rest of these recipes
+    }
+
+
+
+    private static void cactusChesRecipe() {
+        ShapedRecipe cacChes = new ShapedRecipe(cactusArmor.ches);
+
+        cacChes.shape("^ ^", "^^^", "^^^");
+        cacChes.setIngredient('^', new MaterialData(Material.CACTUS, (byte) 1));
+        Bukkit.getServer().addRecipe(cacChes);
+    }
+
+    private static void PetheriteChesRecipe() {
+        ShapedRecipe PChes = new ShapedRecipe(PetheriteSet.ches);
+
+        PChes.shape("^ ^", "^^^", "^^^");
+        PChes.setIngredient('^', new MaterialData(Material.OBSIDIAN, (byte) 1));
+        Bukkit.getServer().addRecipe(PChes);
+    }
+
+    private static void diaChesRecipe() {
+        ShapedRecipe diaChes = new ShapedRecipe(overridenRecipeItems.diaChes);
+
+        diaChes.shape("^ ^", "^^^", "^^^");
+        diaChes.setIngredient('^', Material.DIAMOND);
+        Bukkit.getServer().addRecipe(diaChes);
+    }
+
+    private static void ironChesRecipe() {
+        ShapedRecipe ironChes = new ShapedRecipe(overridenRecipeItems.ironChes);
+
+        ironChes.shape("^ ^", "^^^", "^^^");
+        ironChes.setIngredient('^', Material.IRON_INGOT);
+        Bukkit.getServer().addRecipe(ironChes);
+    }
+
+
+
+    private static void cactusLegRecipe() {
+        ShapedRecipe cacLeg = new ShapedRecipe(cactusArmor.leg);
+
+        cacLeg.shape("^^^", "^ ^", "^ ^");
+        cacLeg.setIngredient('^', new MaterialData(Material.CACTUS, (byte) 1));
+        Bukkit.getServer().addRecipe(cacLeg);
+    }
+
+    private static void PetheriteLegRecipe() {
+        ShapedRecipe PLeg = new ShapedRecipe(PetheriteSet.leg);
+
+        PLeg.shape("^^^", "^ ^", "^ ^");
+        PLeg.setIngredient('^', new MaterialData(Material.OBSIDIAN, (byte) 1));
+        Bukkit.getServer().addRecipe(PLeg);
+    }
+
+    private static void diaLegRecipe() {
+        ShapedRecipe diaLeg = new ShapedRecipe(overridenRecipeItems.diaLeg);
+
+        diaLeg.shape("^^^", "^ ^", "^ ^");
+        diaLeg.setIngredient('^', Material.DIAMOND);
+        Bukkit.getServer().addRecipe(diaLeg);
+    }
+
+    private static void ironLegRecipe() {
+        ShapedRecipe ironLeg = new ShapedRecipe(overridenRecipeItems.ironLeg);
+
+        ironLeg.shape("^^^", "^ ^", "^ ^");
+        ironLeg.setIngredient('^', Material.IRON_INGOT);
+        Bukkit.getServer().addRecipe(ironLeg);
+    }
+
+
+
+    private static void cactusBootsRecipe() {
+        ShapedRecipe cacBoots = new ShapedRecipe(cactusArmor.boots);
+
+        cacBoots.shape("   ", "^ ^", "^ ^");
+        cacBoots.setIngredient('^', new MaterialData(Material.CACTUS, (byte) 1));
+        Bukkit.getServer().addRecipe(cacBoots);
+    }
+
+    private static void PetheriteBootsRecipe() {
+        ShapedRecipe PBoots = new ShapedRecipe(PetheriteSet.boots);
+
+        PBoots.shape("   ", "^ ^", "^ ^");
+        PBoots.setIngredient('^', new MaterialData(Material.OBSIDIAN, (byte) 1));
+        Bukkit.getServer().addRecipe(PBoots);
+    }
+
+    private static void diaBootsRecipe() {
+        ShapedRecipe diaBoots = new ShapedRecipe(overridenRecipeItems.diaBoots);
+
+        diaBoots.shape("   ", "^ ^", "^ ^");
+        diaBoots.setIngredient('^', Material.DIAMOND);
+        Bukkit.getServer().addRecipe(diaBoots);
+    }
+
+    private static void ironBootsRecipe() {
+        ShapedRecipe ironBoots = new ShapedRecipe(overridenRecipeItems.ironBoots);
+
+        ironBoots.shape("   ", "^ ^", "^ ^");
+        ironBoots.setIngredient('^', Material.IRON_INGOT);
+        Bukkit.getServer().addRecipe(ironBoots);
+    }
+
+
+
+    
+    
+    private static void zeusBoltRecipe() {
+        ShapedRecipe ironBoots = new ShapedRecipe(zeusBolt.zeusThunderBolt);
+
+        ironBoots.shape("   ", "^ ^", "^ ^");
+        ironBoots.setIngredient('^', Material.FIREBALL);
+        Bukkit.getServer().addRecipe(ironBoots);
+    }
+}
---
+++ src/tech/nully/primplug/crafting/overridenRecipes/overridenRecipeItems.java
@@ -0,0 +1,147 @@
+package tech.nully.primplug.crafting.overridenRecipes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import tech.nully.primplug.Armor.baseAttributesAdder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class overridenRecipeItems {
+    
+    public static ItemStack diaChes;
+    public static ItemStack diaHelm;
+    public static ItemStack diaLeg;
+    public static ItemStack diaBoots;
+    public static ItemStack diaSword;
+    public static ItemStack diaAxe;
+    public static ItemStack ironHelm;
+    public static ItemStack ironChes; 
+    public static ItemStack ironLeg;
+    public static ItemStack ironBoots;
+    public static ItemStack ironSword;
+    public static ItemStack stoneAxe;
+    public static ItemStack stoneSword;
+    public static ItemStack woodAxe;
+    public static ItemStack woodSword;
+
+    public static void init() {
+        createDiaHelm();
+        createDiaChestplate();
+        createDiaLeggings();
+        createDiaBoots();
+    }
+
+    static baseAttributesAdder b = new baseAttributesAdder();
+    private static void createDiaHelm() {
+        // DEFINE THE META -------------
+        ItemStack DiaHelm = new ItemStack(Material.DIAMOND_HELMET);
+        ItemMeta DiaHelmMeta = DiaHelm.getItemMeta();
+
+
+        // SET THE META ----------------
+        DiaHelmMeta.setDisplayName("Diamond Helmet");
+            // HelmLore
+        List<String> Helmlore = b.addAttributes(DiaHelm, 1, 0, 13, 10, 15, 15);
+        Helmlore.add(ChatColor.RED + "Damage: " + ChatColor.GRAY + "1");
+        Helmlore.add(ChatColor.GREEN + "Defense: " + ChatColor.GRAY + "13");
+        Helmlore.add(ChatColor.BLUE + "Mana: " + ChatColor.GRAY + "15");
+        Helmlore.add(ChatColor.GOLD + "Stamina: " + ChatColor.GRAY + "15");
+        Helmlore.add("");
+        Helmlore.add("The good ol' vanilla diamond armor!");
+        Helmlore.add("");
+        Helmlore.add(ChatColor.BLUE + "" + ChatColor.BOLD + "RARE");
+        DiaHelmMeta.setLore(Helmlore);
+
+        DiaHelm.setItemMeta(DiaHelmMeta);
+        diaHelm = DiaHelm;
+    }
+
+    // ! Cac Chesplate
+    private static void createDiaChestplate() {
+        // DEFINE THE META -------------
+        ItemStack DiaChest = new ItemStack(Material.DIAMOND_CHESTPLATE);
+        ItemMeta DiaChestMeta = DiaChest.getItemMeta();
+
+
+        // SET THE META ----------------
+        DiaChestMeta.setDisplayName("Diamond Chestplate");
+            // ChestLore
+        List<String> Chestlore = new ArrayList<>();
+        Chestlore.add("");
+        Chestlore.add(ChatColor.RED+ "Damage: " + ChatColor.GRAY + "1");
+        Chestlore.add(ChatColor.GREEN + "Defense: " + ChatColor.GRAY + "26");
+        Chestlore.add(ChatColor.BLUE + "Mana: " + ChatColor.GRAY + "15");
+        Chestlore.add(ChatColor.GOLD + "Stamina: " + ChatColor.GRAY + "15");
+        Chestlore.add("");
+        Chestlore.add(ChatColor.LIGHT_PURPLE + "The good ol' vanilla diamond armor!");
+        Chestlore.add("");
+        Chestlore.add(ChatColor.BLUE + "" + ChatColor.BOLD + "RARE");
+        DiaChestMeta.setLore(Chestlore);
+
+        // DIAMOND ARMOR META
+        DiaChest.setItemMeta(DiaChestMeta);
+        diaChes = DiaChest;
+    }
+
+
+    // ! Cac Leggings
+    private static void createDiaLeggings() {
+        // DEFINE THE META -------------
+        ItemStack DiaLeg = new ItemStack(Material.DIAMOND_LEGGINGS);
+        ItemMeta DiaLegMeta = DiaLeg.hasItemMeta() ? DiaLeg.getItemMeta() : Bukkit.getItemFactory().getItemMeta(DiaLeg.getType());
+
+
+        // SET THE META ----------------
+        DiaLegMeta.setDisplayName("Diamond Chestplate");
+            // LegLore
+        List<String> Leglore = new ArrayList<>();
+        Leglore.add("");
+        Leglore.add(ChatColor.BLUE + "Damage: " + ChatColor.GRAY + "1");
+        Leglore.add(ChatColor.BLUE + "Defense: " + ChatColor.GRAY + "13");
+        Leglore.add(ChatColor.BLUE + "Mana: " + ChatColor.GRAY + "15");
+        Leglore.add(ChatColor.BLUE + "Stamina: " + ChatColor.GRAY + "15");
+        Leglore.add("");
+        Leglore.add(ChatColor.LIGHT_PURPLE + "The good ol' vanilla diamond armor!");
+        Leglore.add("");
+        Leglore.add(ChatColor.BLUE + "" + ChatColor.BOLD + "RARE");
+        DiaLegMeta.setLore(Leglore);
+
+
+
+        DiaLeg.setItemMeta(DiaLegMeta);
+        diaLeg = DiaLeg;
+    }
+    // ! Cac Boots
+    private static void createDiaBoots() {
+        // DEFINE THE META -------------
+        ItemStack DiaBoots = new ItemStack(Material.DIAMOND_BOOTS);
+        ItemMeta DiaBootsMeta = DiaBoots.hasItemMeta() ? DiaBoots.getItemMeta() : Bukkit.getItemFactory().getItemMeta(DiaBoots.getType());
+
+
+        // SET THE META ----------------
+        DiaBootsMeta.setDisplayName("Diamond Boots");
+            // BootsLore
+        List<String> Bootslore = new ArrayList<>();
+        Bootslore.add("");
+        Bootslore.add(ChatColor.BLUE + "Damage: " + ChatColor.GRAY + "1");
+        Bootslore.add(ChatColor.BLUE + "Defense: " + ChatColor.GRAY + "8");
+        Bootslore.add(ChatColor.BLUE + "Mana: " + ChatColor.GRAY + "7");
+        Bootslore.add(ChatColor.BLUE + "Stamina: " + ChatColor.GRAY + "13");
+        Bootslore.add("");
+        Bootslore.add(ChatColor.LIGHT_PURPLE + "The good ol' vanilla diamond armor!");
+        Bootslore.add("");
+        Bootslore.add(ChatColor.BLUE + "" + ChatColor.BOLD + "RARE");
+        DiaBootsMeta.setLore(Bootslore);
+
+
+        DiaBoots.setItemMeta(DiaBootsMeta);
+        diaBoots = DiaBoots;
+    }
+
+    // TODO: put the rest of the overriden recipes here
+
+}
