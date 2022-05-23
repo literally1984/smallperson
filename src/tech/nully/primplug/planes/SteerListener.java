777e06f1c62cb7c3d5a141c67625167a880e90ee
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-23 15:57:29 -0700

    Update SteerListener.java

--- src/tech/nully/primplug/planes/SteerListener.java
+++ src/tech/nully/primplug/planes/SteerListener.java
@@ -3,7 +3,6 @@ package tech.nully.primplug.planes;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.util.Vector;
 import tech.nully.primplug.API.events.VehicleSteerEvent;
 
 public class SteerListener implements Listener {
@@ -14,14 +13,5 @@ public class SteerListener implements Listener {
         if (!(e.getPlayer().getVehicle() instanceof Minecart)) {
             return;
         }
-
-        Minecart m = (Minecart) e.getPlayer().getVehicle();
-
-        Vector v = m.getVelocity();
-
-        if (e.getKey().equals("w")) {
-            v = v.add(new Vector(0, 20, 0));
-        }
-
     }
 }
