0e690b37c981a9066cda512be173a93a58cee484
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-05-30 13:25:26 -0700

    Created the WeaponGainEvent

--- src/tech/nully/primplug/API/events/WeaponEXP/WeaponEXPGainEvent.java
+++ src/tech/nully/primplug/API/events/WeaponEXP/WeaponEXPGainEvent.java
@@ -1,7 +1,56 @@
 package tech.nully.primplug.API.events.WeaponEXP;
 
-public class WeaponEXPGainEvent {
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 
+public class WeaponEXPGainEvent extends Event implements Cancellable {
+    private ItemStack item;
+    private int EXP;
+    private Player Owner;
+    private boolean isCancelled;
 
-    public void
+    public WeaponEXPGainEvent(ItemStack EXPItem, int EXP, Player Owner) {
+        this.item = EXPItem;
+        this.EXP = EXP;
+        this.Owner = Owner;
+        this.isCancelled = false;
+    }
+
+    // creates getters for the private variables
+
+    public ItemStack getItem() {
+        return this.item;
+    }
+
+    public int getEXP() {
+        return this.EXP;
+    }
+
+    public Player getOwner() {
+        return this.Owner;
+    }
+    
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean arg0) {
+        this.isCancelled = arg0;
+    }
+
+    private static final HandlerList handlers = new HandlerList();
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 }
