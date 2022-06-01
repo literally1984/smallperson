6dcff5369b7faecfbb572039cf559589f638d36b
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-06-01 06:04:39 -0700

    Converted API

--- src/main/kotlin/tech/nully/primplug/API/PrimItems/PrimItem.kt
+++ src/main/kotlin/tech/nully/primplug/API/PrimItems/PrimItem.kt
@@ -6,12 +6,14 @@ import org.bukkit.inventory.ItemStack
 
 private var PrimItemHashMap: HashMap<ItemStack, PrimItem> = HashMap<ItemStack, PrimItem>()
 data class PrimItem(val primItem: ItemStack) {
-    // Dont you need another class here?
-    // idk, i saw it in kotlin manuals or hdfiosfoj
-    // whats the point of putting another class here?
-    fun hi(){
-        if(){
+    val EXP = i.getItemMeta().getLore().get(i.getItemMeta().getLore().size() -3).split("/")[];
+    fun getPrimItem(primItem: ItemStack): PrimItem? {
+        if (PrimItemHashMap.containsKey(primItem)) {
+            return PrimItemHashMap.get(primItem);
+        }
+        else {
 
+            return this;
         }
     }
 }
---
+++ src/main/kotlin/tech/nully/primplug/API/events/WeaponEXP/WeaponEXPGainEvent.kt
@@ -0,0 +1,6 @@
+package tech.nully.primplug.API.events.WeaponEXP
+
+import org.bukkit.inventory.ItemStack
+
+data class WeaponEXPGainEvent(val item:ItemStack) {
+}
