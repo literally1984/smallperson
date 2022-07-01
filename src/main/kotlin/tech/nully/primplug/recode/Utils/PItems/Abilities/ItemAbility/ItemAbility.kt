7983500a6a9150bc66d6a2a60bb482090e02bd14
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-07-01 12:51:06 -0700

    Update ItemAbility.kt

--- src/main/kotlin/tech/nully/primplug/recode/Utils/PItems/Abilities/ItemAbility/ItemAbility.kt
+++ src/main/kotlin/tech/nully/primplug/recode/Utils/PItems/Abilities/ItemAbility/ItemAbility.kt
@@ -1,8 +1,13 @@
 package tech.nully.primplug.recode.Utils.PItems.Abilities.ItemAbility
 
 enum class ItemAbility {
+    // Buffs
     SpikySpikes,
+    HardenedPlates,
+
     DoubleEdge,
     Vibro,
-    
+
+    // Debuffs
+    GappedArmor
 }
