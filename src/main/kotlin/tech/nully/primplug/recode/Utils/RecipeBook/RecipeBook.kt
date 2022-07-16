e80f3a3e4ab312b925d06e2a5a57391e99192efe
BongoCat <77935781+Fangoboyo@users.noreply.github.com>
2022-07-16 10:19:43 -0700

    Added function to get recipe for an itemstack

--- src/main/kotlin/tech/nully/primplug/recode/Utils/RecipeBook/RecipeBook.kt
+++ src/main/kotlin/tech/nully/primplug/recode/Utils/RecipeBook/RecipeBook.kt
@@ -1,5 +1,7 @@
 package tech.nully.primplug.recode.Utils.RecipeBook
 
+import org.bukkit.Bukkit
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.ShapedRecipe
 
 class RecipeBook {
@@ -8,6 +10,15 @@ class RecipeBook {
         val pageMap = HashMap<ShapedRecipe, RecipePage>()
         fun getRecipePage(page: Int): RecipePage {
             return pages[page + 1]
+        }
+        fun getRecipeFor(item: ItemStack): RecipePage? {
+            val recipe: ShapedRecipe;
+            try {
+                recipe = Bukkit.getRecipesFor(item) as ShapedRecipe
+            } catch (e: NullPointerException) {
+                return null
+            }
+            return pageMap[recipe]
         }
     }
 }
