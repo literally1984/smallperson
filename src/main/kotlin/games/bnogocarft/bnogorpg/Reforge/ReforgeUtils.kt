8e13b8e16f16a0ec5b698557b8fdd52073a63803
BongoCat <77935781+Fangoboyo@users.noreply.github.com>
2022-07-19 23:52:07 -0700

    sorta fixed ayonulls retarded code

--- src/main/kotlin/games/bnogocarft/bnogorpg/Reforge/ReforgeUtils.kt
+++ src/main/kotlin/games/bnogocarft/bnogorpg/Reforge/ReforgeUtils.kt
@@ -3,7 +3,21 @@ package games.bnogocarft.bnogorpg.Reforge
 import games.bnogocarft.bnogorpg.Utils.BItemStack.Reforge
 import org.bukkit.inventory.ItemStack
 
-fun reforge(item: ItemStack){
-    val bweapon = arrayOf(
-        Reforge.YourFather, Reforge.YourMother, Reforge.YourMother, Reforge.YourMother, Reforge.YourMother, Reforge.YourMother, Reforge.YourMother, Reforge.YourMother, Reforge.YourMother, Reforge.YourMother, Reforge.YourSister,Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourSister, Reforge.YourBrother, Reforge.YourBrother, Reforge.YourBrother, Reforge.YourBrother, Reforge.Good, Reforge.Good, Reforge.Good, Reforge.Good, Reforge.Good, Reforge.Good, Reforge.Bad, Reforge.Bad, Reforge.Bad, Reforge.Bad, Reforge.Bad, Reforge.Bad, Reforge.Neutral, Reforge.Neutral, Reforge.Neutral, Reforge.Neutral, Reforge.Neutral, Reforge.Neutral, Reforge.Neutral, Reforge.Neutral,Reforge.Neutral, Reforge.None, Reforge.None, Reforge.None, Reforge.None, Reforge.God, Reforge.God, Reforge.God, Reforge.God, Reforge.God, Reforge.God, Reforge.LitGarbag, Reforge.God, Reforge.God, Reforge.God, Reforge.God, Reforge.LAX1DUDE,Reforge.LAX1DUDE)
+fun Reforge(item: ItemStack) {
+    val weightedList = arrayOf(
+        Reforge.Blessed, Reforge.Blessed, Reforge.Blessed,
+        Reforge.Cursed, Reforge.Cursed, Reforge.Cursed,
+        Reforge.Heavenly,
+        Reforge.Demonic,
+        Reforge.Godly,
+        Reforge.Grim, Reforge.Grim, Reforge.Grim, Reforge.Grim, Reforge.Grim,
+        Reforge.Durable,Reforge.Durable,Reforge.Durable,Reforge.Durable,Reforge.Durable,
+        Reforge.Durable,Reforge.Durable,Reforge.Durable,Reforge.Durable,Reforge.Durable,
+        Reforge.Durable,Reforge.Durable,Reforge.Durable,Reforge.Durable,Reforge.Durable,
+        Reforge.Smart,Reforge.Smart,Reforge.Smart,Reforge.Smart,Reforge.Smart,
+        Reforge.Smart,Reforge.Smart,Reforge.Smart,Reforge.Smart,Reforge.Smart,
+        Reforge.Smart,Reforge.Smart,Reforge.Smart,Reforge.Smart,Reforge.Smart,
+        Reforge.Heavy, Reforge.Heavy, Reforge.Heavy, Reforge.Heavy,
+        Reforge.Heavy, Reforge.Heavy, Reforge.Heavy, Reforge.Heavy,
+    )
 }
