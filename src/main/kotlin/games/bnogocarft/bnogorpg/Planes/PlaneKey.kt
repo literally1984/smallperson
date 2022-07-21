4c3f7525b52686b3aaf67293551fa04ac8333cdf
BongoCat <77935781+Fangoboyo@users.noreply.github.com>
2022-07-20 23:49:21 -0700

    fucked around with vectors, untested

--- src/main/kotlin/games/bnogocarft/bnogorpg/Planes/PlaneKey.kt
+++ src/main/kotlin/games/bnogocarft/bnogorpg/Planes/PlaneKey.kt
@@ -1,10 +1,14 @@
 package games.bnogocarft.bnogorpg.Planes
 
+import games.bnogocarft.bnogorpg.Main
 import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.entity.Minecart
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.scheduler.BukkitTask
+import org.bukkit.util.Vector
 
 val planes = HashMap<Minecart, ItemStack>()
 
@@ -57,7 +61,10 @@ data class PlaneEntity(val key: PlaneKey) {
     var maxHealth = key.stats.maxHealth
     var maxFuel = key.stats.maxFuel
     var isSpawned = false
-     var destroyed = false
+    var isRunning = false
+    var destroyed = false
+    var continueRun: BukkitTask? = null
+
     fun spawn(loc: Location) {
         val planeCart = Minecart::class.java
         planeEntity = loc.world.spawn(loc, planeCart)
@@ -65,11 +72,60 @@ data class PlaneEntity(val key: PlaneKey) {
         planes[planeEntity] = key.item
     }
 
+    fun start(p: Player) {
+        if (p.vehicle.equals(planeEntity)) {
+            planeEntity.derailedVelocityMod = Vector(0.5,0.5,0.5)
+            val velo = Vector(p.location.direction.x, 0.5, p.location.direction.z)
+            planeEntity.velocity = velo
+            var i = 4 // Task will run 10 times.
+            var task: BukkitTask? = null
+
+
+            task = Bukkit.getScheduler().runTaskTimer(Main.instance, {
+                fun run() {
+                    if(i != 0) {
+                        planeEntity.velocity = velo
+                        i--
+                    } else {
+                        task!!.cancel()
+                    }
+                }}, 0, 1)
+
+            val continueVelo = Vector(p.location.direction.x, 0.0, p.location.direction.z)
+            val travelSpeed = speed/20
+            var loopsTilFuel = 20
+            isRunning = true
+            // TODO: make sure Bukkit scheduler isnt multi-threaded
+            continueRun = Bukkit.getScheduler().runTaskTimer(Main.instance, {
+                fun run() {
+                    val destination = Location(p.world, p.location.x + travelSpeed, p.location.y, p.location.z + travelSpeed)
+                    planeEntity.velocity = (destination.subtract(p.location)).toVector()
+                    loopsTilFuel -= 1
+                    if (loopsTilFuel == 0) {
+                        fuel =- 1
+                        loopsTilFuel = 20
+                    }
+                    if (fuel == 0) {
+                        stop()
+                    }
+                }}, 0, 1)
+        }
+    }
+
+    fun stop() {
+        if (isRunning) {
+            isRunning = false
+            continueRun!!.cancel()
+        }
+    }
+
     fun collect() {
-        if (isSpawned) {
-            planeEntity.remove()
-            isSpawned = false
-            planes.remove(planeEntity)
+        if (!isRunning) {
+            if (isSpawned) {
+                planeEntity.remove()
+                isSpawned = false
+                planes.remove(planeEntity)
+            }
         }
     }
 }
