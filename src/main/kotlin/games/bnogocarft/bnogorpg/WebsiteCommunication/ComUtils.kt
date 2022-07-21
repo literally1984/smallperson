e8514982b35d1a3d102e568c0f29abb5cf60b376
primboi <81822069+primitt@users.noreply.github.com>
2022-07-21 09:55:02 -0700

    Commit some basic code

--- src/main/kotlin/games/bnogocarft/bnogorpg/WebsiteCommunication/ComUtils.kt
+++ src/main/kotlin/games/bnogocarft/bnogorpg/WebsiteCommunication/ComUtils.kt
@@ -1,4 +1,17 @@
 package games.bnogocarft.bnogorpg.WebsiteCommunication
 
+import org.java-websocket.server.WebSocketServer
 class ComUtils {
+    // start websocket server
+    fun startWebSocketServer(port: Int) {
+        val server = WebSocketServer(port)
+        server.start()
+    }
+    // make onOpen websocket function
+    fun onOpen(server: WebSocketServer) {
+        server.onOpen = {
+            println("Connection opened")
+        }
+    }
+    startWebSocketServer(8080)
 }
