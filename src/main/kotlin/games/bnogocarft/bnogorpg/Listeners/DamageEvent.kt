318304e0a2c3a6c088f5fc313a68a3bc0665d6a6
BongoCat <bungleberrysqueegybobber@gmail.com>
2022-08-14 23:16:01 -0700

    bar display debugging

--- src/main/kotlin/games/bnogocarft/bnogorpg/Listeners/DamageEvent.kt
+++ src/main/kotlin/games/bnogocarft/bnogorpg/Listeners/DamageEvent.kt
@@ -1,6 +1,7 @@
 package games.bnogocarft.bnogorpg.Listeners
 
 import games.bnogocarft.bnogorpg.Main
+import games.bnogocarft.bnogorpg.PlayerBar.Bar
 import games.bnogocarft.bnogorpg.PlayerBar.CombatLogBar
 import games.bnogocarft.bnogorpg.PlayerBar.ComboBar
 import games.bnogocarft.bnogorpg.Utils.BPlayer.BPlayers
@@ -28,55 +29,67 @@ class DamageEvent : Listener {
 
                 // Checks if the damaged entity is also a player
                 if (e.entity is Player) {
-                    // Starts combat log timer for damager
-                    lateinit var combatLogtask: BukkitTask
-                    bDamager.metadata["combat"] = CombatLog(combatLogtask)
-                    combatLogtask =
-                        Bukkit.getScheduler().runTaskTimer(Main.instance, CombatLogTimer(bDamager), 0, 20)
-                    (e.damager as Player).sendMessage("${ChatColor.RED}You are in Combat! Please do not log out or else")
-                    (e.damager as Player).sendMessage("${ChatColor.RED}you will lose all of your items!")
-                    val cBar = CombatLogBar(5, 300)
-                    bDamager.bars.add(cBar)
-                    var display = true
-                    for (bar in bDamager.bars) {
-                        if (bar.priority > 2) {
-                            display = false
+                    lateinit var combatLogTask: BukkitTask
+                    val combat: CombatLog? = bDamager.metadata["combo"] as CombatLog?
+                    if (combat == null) {// Checks if the player is currently in combat
+                        bDamager.metadata["combat"] = CombatLog(combatLogTask)
+
+                        // Runs a task to decide when the player is going to get out of combat
+                        combatLogTask =
+                            Bukkit.getScheduler().runTaskTimer(Main.instance, CombatLogTimer(bDamager), 0, 20)
+                        (e.damager as Player).sendMessage("${ChatColor.RED}You are in Combat! Please do not log out or else")
+                        (e.damager as Player).sendMessage("${ChatColor.RED}you will lose all of your items!")
+
+                        // Adds a full combat bar to the player's bars
+                        val cBar = CombatLogBar(5, 300)
+                        bDamager.bars.add(cBar)
+
+                        // Checks if anything in the players bars has a higher priority than combat's (2)
+                        var display = true
+                        for (bar in bDamager.bars) {
+                            if (bar.priority > 2) {
+                                display = false
+                            }
                         }
-                    }
-                    if (display) {
-                        bDamager.bar.text = cBar.name
-                        bDamager.bar.health = cBar.health
+
+                        // Displays the player bar if there is
+                        if (display) {
+                            bDamager.bar.text = cBar.name
+                            bDamager.bar.health = cBar.health
+                        }
                     }
 
                     // Starts a combat log timer for the damaged
                     val bDamaged = BPlayers[e.entity]!!
-                    lateinit var combatLogtask2: BukkitTask
-                    bDamaged.metadata["combat"] = CombatLog(combatLogtask)
-                    combatLogtask2 =
-                        Bukkit.getScheduler().runTaskTimer(Main.instance, CombatLogTimer(bDamaged), 0, 20)
-                    (e.damager as Player).sendMessage(
-                        "${ChatColor.RED}You are in Combat! Please do not log out or else"
-                    )
-                    (e.damager as Player).sendMessage(
-                        "${ChatColor.RED}you will lose all of your items!"
-                    )
-                    val cBar2 = CombatLogBar(5, 300)
-                    bDamaged.bars.add(cBar2)
-                    var display2 = true
-                    for (bar in bDamaged.bars) {
-                        if (bar.priority > 2) {
-                            display2 = false
+                    val combat2: CombatLog? = bDamaged.metadata["combo"] as CombatLog?
+                    if (combat2 != null) {
+                        lateinit var combatLogTask2: BukkitTask
+                        bDamaged.metadata["combat"] = CombatLog(combatLogTask)
+                        combatLogTask2 =
+                            Bukkit.getScheduler().runTaskTimer(Main.instance, CombatLogTimer(bDamaged), 0, 20)
+                        (e.damager as Player).sendMessage(
+                            "${ChatColor.RED}You are in Combat! Please do not log out or else"
+                        )
+                        (e.damager as Player).sendMessage(
+                            "${ChatColor.RED}you will lose all of your items!"
+                        )
+                        val cBar2 = CombatLogBar(5, 300)
+                        bDamaged.bars.add(cBar2)
+                        var display2 = true
+                        for (bar in bDamaged.bars) {
+                            if (bar.priority > 2) {
+                                display2 = false
+                            }
                         }
-                    }
-                    if (display2) {
-                        bDamaged.bar.text = cBar2.name
-                        bDamaged.bar.health = cBar2.health
+                        if (display2) {
+                            bDamaged.bar.text = cBar2.name
+                            bDamaged.bar.health = cBar2.health
+                        }
                     }
                 }
 
                 // Starts a Combo for the damager
                 if (bDamager.combo == null) { // If the player is not currently comboing
-                    bDamager.bar.text = "Combo Damage: ${e.damage}"
                     val comboTask = Bukkit.getScheduler().runTaskTimer(Main.instance, ComboTimer(bDamager), 0, 1)
                     bDamager.combo = Combo(comboTask, 5, e.damage)
 
@@ -89,18 +102,31 @@ class DamageEvent : Listener {
                         }
                     }
                     if (display) {
+                        bDamager.currentBar = coBar
                         bDamager.bar.text = coBar.name
                         bDamager.bar.health = coBar.health
                     }
                 } else { // If the player is comboing
-                    for (bar in bDamager.bars) {
-                        if (bar is ComboBar) {
-                            bar.name = "Combo Damage: ${e.damage + bDamager.combo!!.damage}"
-                            bar.health = 300
-                        }
-                    }
                     bDamager.combo!!.damage += e.damage
                     bDamager.combo!!.timeLeft = 5
+                    var display = true
+                    lateinit var dBar: Bar
+                    for (bar in bDamager.bars) {
+                        if (bar is ComboBar) {
+                            bar.health = 300
+                            bar.name = "Combo Damage: ${bar.combo.damage}"
+                            dBar = bar
+                        }
+                        if (bar.priority > 2) {
+                            display = false
+                        }
+                    }
+
+                    if (display) {
+                        bDamager.currentBar = dBar
+                        bDamager.bar.text = dBar.name
+                        bDamager.bar.health = dBar.health
+                    }
                 }
             }
 
--- src/main/kotlin/games/bnogocarft/bnogorpg/PlayerBar/ComboBar.kt
+++ src/main/kotlin/games/bnogocarft/bnogorpg/PlayerBar/ComboBar.kt
@@ -2,7 +2,7 @@ package games.bnogocarft.bnogorpg.PlayerBar
 
 import games.bnogocarft.bnogorpg.combat.ComboCounter.Combo
 
-class ComboBar(combo: Combo, override var health: Int) : Bar {
+data class ComboBar(val combo: Combo, override var health: Int) : Bar {
     override var name: String = "Combo Damage: ${combo.damage}"
     override var priority: Int = 3
 }
