cad1c449df0d19d6b90f0c1242376aa6c30776db
BongoCat <bungleberrysqueegybobber@gmail.com>
2022-12-06 06:48:00 -0800

    screw that sql libary i created im using exposed now

---
+++ src/main/kotlin/me/bnogocarft/bnogorpg/database/BnogoSQL.kt
@@ -0,0 +1,46 @@
+package me.bnogocarft.bnogorpg.database
+
+import ArrayColumnType
+import array
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.*
+
+enum class ClassType
+
+class BnogoSQL {
+
+    companion object {
+        var con: Connection? = null
+        fun enable() {
+            Database.connect(
+                "jdbc:postgresql://ep-noisy-mouse-090229.eu-central-1.aws.neon.tech/neondb",
+                user = "Fangoboyo",
+                password = "Dhsx56YPfiVr",
+                driver = "org.postgresql.Driver"
+            )
+            transaction {
+                addLogger(StdOutSqlLogger)
+                SchemaUtils.create(players)
+            }
+            print("connection successful")
+        }
+
+        object players: Table() {
+            val stash = array<ByteArray>("stash", BinaryColumnType(100000))
+            val name = varchar("name", 50)
+            val talismans = array<ByteArray>("talismans", BinaryColumnType(100000))
+            val spells = array<ByteArray>("spells", BinaryColumnType(100000))
+            val skill_stats = array<Float>("skill_stats", FloatColumnType())
+        }
+
+        object auctions: Table() {
+            val seller = varchar("seller_name", 50)
+            val endTime = integer("end_time")
+            val bidder = varchar("current_bidder", 50)
+            val bid = integer("current_bid")
+            val item = binary("item", 100000)
+            val id = integer("id")
+        }
+    }
+}
